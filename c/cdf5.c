118141d633dc44d1d2f63334d46c734280ef7165
Charlie Zender <zender@uci.edu>
2017-08-21 17:13:50 -0700

    lazy commit

--- c/cdf5.c
+++ c/cdf5.c
@@ -1,4 +1,5 @@
 // Purpose: Test behavior of large CDF5 files
+// 20170821: Original test by Wei-keng Liou 
 
 // gcc -std=c99 -I/opt/local/include -o ~/bin/cdf5 ~/sw/c/cdf5.c -L/opt/local/lib -lnetcdf -lhdf5_hl -lhdf5 -lcurl
 
@@ -12,45 +13,52 @@
 
 int main(int argc, char *argv[])
 {
-    int err, nerrs=0, ncid, dimid, varid[3], int_buf1, int_buf2;
-    size_t i;
-    double *buf, avg=0.0;
+  int err, nerrs=0, ncid, dimid, varid[3], int_buf1, int_buf2;
+  size_t i, cnt[1],srt[1];
+  double *buf, avg=0.0;
+  
+  err = nc_create("cdf5.nc", NC_CLOBBER|NC_CDF5, &ncid); ERR
+  err = nc_def_dim(ncid, "dim", DIM, &dimid); ERR
+  err = nc_def_var(ncid, "one", NC_INT, 0, NULL, &varid[1]); ERR
+  err = nc_def_var(ncid, "var", NC_DOUBLE, 1, &dimid, &varid[0]); ERR
+  err = nc_def_var(ncid, "two", NC_INT, 0, NULL, &varid[2]); ERR
+  err = nc_set_fill(ncid, NC_NOFILL, NULL); ERR
+  err = nc_enddef(ncid); ERR
 
-    err = nc_create("test.nc", NC_CLOBBER|NC_CDF5, &ncid); ERR
-    err = nc_def_dim(ncid, "dim", DIM, &dimid); ERR
-    err = nc_def_var(ncid, "var", NC_DOUBLE, 1, &dimid, &varid[0]); ERR
-    err = nc_def_var(ncid, "one", NC_INT, 0, NULL, &varid[1]); ERR
-    err = nc_def_var(ncid, "two", NC_INT, 0, NULL, &varid[2]); ERR
-    err = nc_set_fill(ncid, NC_NOFILL, NULL); ERR
-    err = nc_enddef(ncid); ERR
+  int_buf1 = 1;
+  err = nc_put_var_int(ncid, varid[1], &int_buf1); ERR
 
-    buf = (double*) malloc(DIM * sizeof(double));
-    for (i=0; i<DIM; i++) buf[i] = 1.0;
+  buf = (double*) malloc(DIM * sizeof(double));
+  for (i=0; i<DIM; i++) buf[i] = 1.0;
+  //err = nc_put_var_double(ncid, varid[0], buf); ERR
+  srt[0]=0;
+  cnt[0]=DIM;
+  err = nc_put_vara_double(ncid,varid[0],srt,cnt, buf); ERR
 
-    err = nc_put_var_double(ncid, varid[0], buf); ERR
-    int_buf1 = 1;
-    err = nc_put_var_int(ncid, varid[1], &int_buf1); ERR
-    int_buf2 = 2;
-    err = nc_put_var_int(ncid, varid[2], &int_buf2); ERR
-    err = nc_close(ncid); ERR
+  int_buf2 = 2;
+  err = nc_put_var_int(ncid, varid[2], &int_buf2); ERR
+  err = nc_close(ncid); ERR
 
-    err = nc_open("cdf5.nc", NC_NOWRITE, &ncid); ERR
-    err = nc_inq_varid(ncid, "var", &varid[0]); ERR
-    err = nc_inq_varid(ncid, "one", &varid[1]); ERR
-    err = nc_inq_varid(ncid, "two", &varid[2]); ERR
-    for (i=0; i<DIM; i++) buf[i] = 0.0;
-    err = nc_get_var_double(ncid, varid[0], buf); ERR
-    int_buf1 = int_buf2 = 0;
-    err = nc_get_var_int(ncid, varid[1], &int_buf1); ERR
-    err = nc_get_var_int(ncid, varid[2], &int_buf2); ERR
-    err = nc_close(ncid); ERR
+  err = nc_open("cdf5.nc", NC_NOWRITE, &ncid); ERR
+  err = nc_inq_varid(ncid, "one", &varid[1]); ERR
+  err = nc_inq_varid(ncid, "var", &varid[0]); ERR
+  err = nc_inq_varid(ncid, "two", &varid[2]); ERR
+  int_buf1 = int_buf2 = 0;
+  err = nc_get_var_int(ncid, varid[1], &int_buf1); ERR
+  for (i=0; i<DIM; i++) buf[i] = 0.0;
+  //  err = nc_get_var_double(ncid, varid[0], buf); ERR
+  srt[0]=0;
+  cnt[0]=DIM;
+  err = nc_get_vara_double(ncid,varid[0],srt,cnt, buf); ERR
+  err = nc_get_var_int(ncid, varid[2], &int_buf2); ERR
+  err = nc_close(ncid); ERR
 
-    printf("get var one = %d\n",int_buf1);
-    printf("get var two = %d\n",int_buf2);
-    for (i=0; i<DIM; i++) avg += buf[i];
-    avg /= DIM;
-    printf("avg = %f\n",avg);
-    free(buf);
+  printf("get var one = %d\n",int_buf1);
+  printf("get var two = %d\n",int_buf2);
+  for (i=0; i<DIM; i++) avg += buf[i];
+  avg /= DIM;
+  printf("avg = %f\n",avg);
+  free(buf);
 
-    return (nerrs > 0);
+  return (nerrs > 0);
 }
