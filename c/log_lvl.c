a3e8ed5dd9fcb3be1040d7bcea34c26dff8b2d78
Charlie Zender <zender@uci.edu>
2018-02-07 10:25:03 -0800

    Add log_lvl.c

---
+++ c/log_lvl.c
@@ -0,0 +1,18 @@
+// Purpose: Test nc_set_log_level()
+
+// gcc -std=c99 -I/opt/local/include -o ~/bin/log_lvl ~/sw/c/log_lvl.c -L/opt/local/lib -lnetcdf -lhdf5_hl -lhdf5 -lcurl
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <netcdf.h>
+
+#define ERR {if(err!=NC_NOERR){printf("Error at line %d in %s: %s\n", __LINE__,__FILE__, nc_strerror(err));nerrs++;}}
+
+int main(int argc, char *argv[])
+{
+  int ncid;
+  nc_set_log_level(3);
+  nc_create("file.nc", NC_NETCDF4, &ncid);
+  nc_close(ncid);
+}
+
