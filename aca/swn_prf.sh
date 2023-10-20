92e04da9710474106abdc725c06157c35b0773b9
Charlie Zender <zender@uci.edu>
2023-10-20 11:56:47 -0700

    Add swn_rrtmgp_bnd5.nco, swn_prf.sh to simulate/diagnose VIS+NIR components of RRTMG Band 5. Results are believable.

--- aca/swn_prf.sh
+++ aca/swn_prf.sh
@@ -10,26 +10,35 @@ drc_in=${DATA_RT}
 drc_out=${DATA}/rrtmgp/swnb2
 spt_src="${BASH_SOURCE[0]}"
 spt_nm=$(basename ${spt_src}) # [sng] Script name (unlike $0, ${BASH_SOURCE[0]} works well with 'source <script>')
+
+# Main loop over profiles
 #for prf in mls mlw sas saw std tro; do
 for prf in tro; do
     mkdir -p ${drc_out}
+
+    # Select surface reflectance appropriate for profile
     if [ ${prf} = 'tro' ]; then
        rfl_sfc=rfl_spc_sfc_snc_100um_csza0.5.nc
     else
        rfl_sfc=rfl_spc_sfc_ocn_chl0.0_csza0.5.nc
     fi # !prf
+
     # Simulate atmosphere
     cmd_swn="swnb2 --drc_in=${drc_in} --lqd=aer_h2o_lqd_rds_swa_10.nc --prf=${prf}_icrccm_50lvl.nc --rfl=${rfl_sfc} --out=${drc_out}/swn_${prf}_clr.nc > ${drc_out}/swnb2_${prf}_clr.txt 2>&1"
     eval ${cmd_swn}
     if [ "$?" -ne 0 ]; then
-printf "${spt_nm}: ERROR Failed to simulate atmosphere. Debug this:\n${cmd_swn}\n"
-exit 1
+	printf "${spt_nm}: ERROR Failed to simulate atmosphere. Debug this:\n${cmd_swn}\n"
+	exit 1
     fi # !err
+
     # Re-order wavelengths (optional)
     cmd_pdq="ncpdq -O --rdr=-bnd ${drc_out}/swn_${prf}_clr.nc ${drc_out}/swn_${prf}_clr.nc"
     eval ${cmd_pdq}
     if [ "$?" -ne 0 ]; then
-printf "${spt_nm}: ERROR Failed to reverse dimensions. Debug this:\n${cmd_pdq}\n"
-exit 1
+	printf "${spt_nm}: ERROR Failed to reverse dimensions. Debug this:\n${cmd_pdq}\n"
+	exit 1
     fi # !err
-done
+
+done # !prf
+
+exit 0
